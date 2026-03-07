8223a28639bf360d6ab05537b3aef6c69cdec86c
Hiraina <hei.hiraina@gmail.com>
2026-03-07 13:16:55 +0300

    feat: add hero section and update navbar style

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,27 @@
+import profil from "../assets/images/profil.jpg"
+import "./hero.css"
+
 function Hero() {
     return (
-        <></>
+        <section className="pt-[200px] relative flex flex-row justify-between items-center ml-[10%] mr-[10%]">
+            <div className="content flex flex-col gap-10">
+                <h1 className="content-title text-5xl text-white font-semibold tracking-widest">Hello, I'm Hiraina ...</h1>
+                <p className="text-white text-2xl mt-4 max-w-[90%]">
+                    Full-stack Java developer, passionate about building modern web applications with React and Spring Boot.
+                </p>
+                <div className="buttons w-full text-xl text-white flex flex-row gap-8 max-w-[90%]">
+                    <button className="contact-button flex-1 flex flex-row justify-center items-center gap-4 py-4 rounded-full">
+                        <i class="fa-regular fa-envelope "></i>
+                        <span className="font-semibold">contact me</span>
+                    </button>
+                    <button className="download-cv-button flex-1 flex flex-row justify-center items-center gap-4 py-4 rounded-full">
+                        <i class="fa-solid fa-download"></i>
+                        <span className="font-semibold">Download CV</span>
+                    </button>
+                </div>
+            </div>
+            <img src={profil} alt="profil_img" className="profil w-[350px] h-[350px] rounded-full border-4 border-white" />
+        </section>
     )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,20 +2,24 @@ import "./navbar.css"
 
 function Navbar() {
     return (
-        <header className="header fixed top-2 left-1/2 w-[50%] flex justify-center transform -translate-x-1/2 backdrop-blur-md rounded-lg">
-            <nav className="flex flex-row w-full justify-between items-center px-8 py-2">
-                <img src="../../public/mada_flag.jpg" alt="mada_flag_img" className="h-full w-[50px]" />
-                <ul className="flex justify-center items-center gap-10 text-white text-xl font-bold">
-                    <li>
+        <header className="header fixed top-3 left-1/2 w-[80%] border-2 border-white flex justify-center transform -translate-x-1/2 backdrop-blur-md rounded-full">
+            <nav className="flex flex-row w-full justify-between items-center px-8 py-4">
+                <img src="/mada_flag.jpg" alt="mada_flag_img" className="h-full w-[50px]" />
+                <ul className="flex justify-center items-center gap-16 text-white text-xl font-semibold">
+                    <li className="flex flex-row justify-center items-center gap-2">
+                        <i class="fa-solid fa-house"></i>
                         <a href="">Hero</a>
                     </li>
-                    <li>
+                    <li className="flex flex-row justify-center items-center gap-2">
+                        <i class="fa-solid fa-circle-info"></i>
                         <a href="">About</a>
                     </li>
-                    <li>
+                    <li className="flex flex-row justify-center items-center gap-2">
+                        <i class="fa-solid fa-code"></i>
                         <a href="">Skills</a>
                     </li>
-                    <li>
+                    <li className="flex flex-row justify-center items-center gap-2">
+                        <i class="fa-solid fa-folder-closed"></i>
                         <a href="">Projects</a>
                     </li>
                 </ul>
