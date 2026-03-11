cdacef5f18efd7b9effd042df02b294b8213e423
Hiraina <hei.hiraina@gmail.com>
2026-03-11 11:01:40 +0300

    change text size to xs for TechItem

--- src/components/skills/TechItem.jsx
+++ src/components/skills/TechItem.jsx
@@ -4,7 +4,7 @@ function TechItem({ name, icon }) {
     return (
         <div className="tech-item h-[120px] w-[120px] flex flex-col items-center justify-center gap-2 shadow-lg p-10 rounded-full">
             {icon && <img src={icon} alt={name} className="tech-icon h-[60px] w-[60px]" />}
-            <span className="text-white font-semibold">{name}</span>
+            <span className="text-white text-xs font-semibold">{name}</span>
         </div>
     )
 }
